5b59ceee325adfe664ce4af43a0c6a740267ca84
Loperhomneto <anshultheawesome@gmail.com>
2022-08-18 13:51:08 -0700

    custom tags and more minor fixes ++

--- src/main/java/net/darky/pappimod/item/custom/DowsingRodItem.java
+++ src/main/java/net/darky/pappimod/item/custom/DowsingRodItem.java
@@ -2,13 +2,21 @@ package net.darky.pappimod.item.custom;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.item.TooltipContext;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUsageContext;
 import net.minecraft.text.LiteralText;
+import net.minecraft.text.Text;
 import net.minecraft.text.TranslatableText;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class DowsingRodItem extends Item {
 
@@ -34,7 +42,7 @@ public class DowsingRodItem extends Item {
             }
 
             if(!foundBlock) {
-                player.sendMessage(new TranslatableText("item.tutorialmod.dowsing_rod.no_valuables"), false);
+                player.sendMessage(new TranslatableText("item.pappimod.dowsing_rod.no_valuables"), false);
             }
         }
 
@@ -44,13 +52,23 @@ public class DowsingRodItem extends Item {
         return super.useOnBlock(context);
     }
 
+    @Override
+    public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
+        if(Screen.hasShiftDown()) {
+            tooltip.add(new TranslatableText("item.pappimod.dowsing_rod.tooltip.shift"));
+        } else {
+            tooltip.add(new TranslatableText("item.pappimod.dowsing_rod.tooltip"));
+        }
+    }
+
     private void outputValuableCoordinates(BlockPos blockPos, PlayerEntity player, Block blockBelow) {
         player.sendMessage(new LiteralText("Found " + blockBelow.asItem().getName().getString() + " at " +
-                "(" + blockPos.getX() + ", " + blockPos.getY() + "," + blockPos.getZ() + ")"), false);
+                "(" + blockPos.getX() + ", " + blockPos.getY() + ", " + blockPos.getZ() + ")"), false);
     }
 
     private boolean isValuableBlock(Block block) {
-        return block == Blocks.COAL_ORE || block == Blocks.COPPER_ORE
-                || block == Blocks.DIAMOND_ORE || block == Blocks.IRON_ORE;
+        return block == Blocks.COAL_ORE || block == Blocks.GOLD_ORE || block == Blocks.LAPIS_ORE || block == Blocks.DIAMOND_ORE || block == Blocks.IRON_ORE
+                || block == Blocks.DEEPSLATE_COAL_ORE || block == Blocks.DEEPSLATE_GOLD_ORE || block == Blocks.DEEPSLATE_LAPIS_ORE
+                || block == Blocks.DEEPSLATE_DIAMOND_ORE || block == Blocks.DEEPSLATE_IRON_ORE;
     }
 }
