cb863853ff8e6f07115ef00004fac87b663ac8d1
Loperhomneto <anshultheawesome@gmail.com>
2022-08-18 13:18:51 -0700

    minor fixes y

--- src/main/java/net/darky/pappimod/block/ModBlocks.java
+++ src/main/java/net/darky/pappimod/block/ModBlocks.java
@@ -16,13 +16,13 @@ import net.minecraft.util.registry.Registry;
 public class ModBlocks {
 
     public static final Block MYTHRIL_BLOCK =
-            registerBlock("mythril_block", new Block(FabricBlockSettings.of(Material.METAL).strength(6f).requiresTool()), ModItemGroup.MYTHRIL);
+            registerBlock("mythril_block", new Block(FabricBlockSettings.of(Material.METAL).strength(5.5f).requiresTool()), ModItemGroup.MYTHRIL);
     public static final Block MYTHRIL_ORE =
             registerBlock("mythril_ore", new Block(FabricBlockSettings.of(Material.STONE).strength(4.5f).requiresTool()), ModItemGroup.MYTHRIL);
     public static final Block RAW_MYTHRIL_BLOCK =
             registerBlock("raw_mythril_block", new Block(FabricBlockSettings.of(Material.STONE).strength(5f).requiresTool()), ModItemGroup.MYTHRIL);
     public static final Block DEEPSLATE_MYTHRIL_ORE =
-            registerBlock("deepslate_mythril_ore", new Block(FabricBlockSettings.of(Material.STONE).strength(8f).requiresTool()), ModItemGroup.MYTHRIL);
+            registerBlock("deepslate_mythril_ore", new Block(FabricBlockSettings.of(Material.STONE).strength(6f).requiresTool()), ModItemGroup.MYTHRIL);
     public static final Block NETHERRACK_MYTHRIL_ORE =
             registerBlock("netherrack_mythril_ore", new Block(FabricBlockSettings.of(Material.STONE).strength(3.5f).requiresTool()), ModItemGroup.MYTHRIL);
 
